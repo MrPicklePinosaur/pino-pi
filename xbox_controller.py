bfde9906c7aa155701df401ba806641de053f9ff
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-12-08 21:47:51 -0500

    get controller input

--- xbox_controller.py
+++ xbox_controller.py
@@ -11,15 +11,30 @@ for i in range(pygame.joystick.get_count()):
 
 	print("Initializing joystick "+joysticks[-1].get_name())
 
-
 try:
 	while True:
+		'''
 		for evt in pygame.event.get():
 			
 			if evt.type == pygame.JOYAXISMOTION:
                             print("YEEET")
-			
+        '''
 
+        for j in joysticks:
+
+        	for axis in range(j.get_numaxes()):
+        		print("Axis #"+axis+": "+j.get_axis(axis)) 
+
+        	for ball in range(j.get_numballs()):
+        		print("Ball #"+ball+": "+j.get_ball(ball)) 
+
+        	for button in range(j.get_numbuttons()):
+        		print("Button #"+button+": "+j.get_button(button)) 
+
+        	for hat in range(j.get_numhats()):
+        		print("Hat #"+hat+": "+j.get_hat(hat)) 
+
+        	print("=-=-=-=-=-=-=-=-=")
 
 except KeyboardInterrupt:
 	pass
